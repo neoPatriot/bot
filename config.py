5bd9ee357ff01694fa12d1b6e2013e34e105152c
Aleksey Utkin <neopatriot@mail.ru>
2025-08-17 19:38:53 +0300

    nnn

---
+++ config.py
@@ -0,0 +1,31 @@
+from telegram import KeyboardButton
+
+# Конфигурационные константы
+TOKEN = "7641941550:AAHI8Jbp_oWPCUXh92wm1tL9JmbzuaPhigg"  # Замените на ваш токен
+
+ADMIN_USER_IDS = [1098521522, 1042197487, 1928520826, 810564487]
+ROOM_ADMINS = {
+    1: [],  # Администраторы для комнаты 1
+    2: [],  # Администраторы для комнаты 2
+    3: [],  # Администраторы для комнаты 3
+    4: [],  # Администраторы для комнаты 4
+    5: []   # Администраторы для комнаты 5
+}
+
+ROOM_NAMES = {
+    1: 'Чертог "Z-Studio"',
+    3: '"Кузня" РОК-школы "Z-school"',
+    4: '"Певческая" РОК-школы "Z-school"',
+    5: '"Гитарные покои" РОК-школы "Z-school"',
+}
+
+API_BASE_URL = "https://deadprogrammer.ru/all"
+BOOKING_BASE_URL = "https://бигзэтика.рф/book"
+
+# Главное меню
+MAIN_MENU_KEYBOARD = [
+    [KeyboardButton("Просмотр расписания"), KeyboardButton("Бронировать")],
+    [KeyboardButton("❓ Помощь"), KeyboardButton("ℹ️ О боте")]
+]
+
+# Меню просмотра расписания больше не используется
